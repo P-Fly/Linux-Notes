fdcce3317f696ef158e4089f6120098a00c86c98
P-Fly <27144363@qq.com>
2020-09-13 10:41:03 +0800

    Add update-alternatives config and running script base on host_env_setup.sh

--- tools/scripts/host_env_setup.sh
+++ tools/scripts/host_env_setup.sh
@@ -20,12 +20,13 @@ fi
 echo "" > host_env_setup.log
 
 echo "Installing apt packages:"
+
 while read line
 do
     # Remove extra whitespace from an ASCII line
     line=`echo "${line}" | sed -e 's/^[ ]*//g' | sed -e 's/[ ]*$//g'`
 
-    # Ignore comments
+    # Ignore comments and blank line
     m=`echo "${line}" | grep -v ^#`
     if [ ! "${m}" ]
     then
@@ -34,18 +35,53 @@ do
 
     # Ignore the already installed software
     dpkg -s "${line}" >>host_env_setup.log 2>&1
-    if [ "$?" == "0" ]
+    if [ "$?" = "0" ]
     then
         echo -e "${line} [\033[32mDone\033[0m]" | awk '{printf "%-50s%-10s\n", $1, $2}'
         continue
     fi
 
     apt-get --yes install "${line}" >>host_env_setup.log 2>&1
-    if [ "$?" == "0" ]
+    if [ "$?" = "0" ]
     then
         echo -e "${line} [\033[32mPass\033[0m]" | awk '{printf "%-50s%-10s\n", $1, $2}'
     else
         echo -e "${line} [\033[31mFailed\033[0m]" | awk '{printf "%-50s%-10s\n", $1, $2}'
-        exit 1        
+        exit 1
     fi
 done < apt_setup.lst
+
+echo "Use update-alternatives create symbolic links determining default commands:"
+
+while read line
+do
+    # Remove extra whitespace from an ASCII line
+    line=`echo "${line}" | sed -e 's/^[ ]*//g' | sed -e 's/[ ]*$//g'`
+
+    # Ignore comments and blank line
+    m=`echo "${line}" | grep -v ^#`
+    if [ ! "${m}" ]
+    then
+        continue
+    fi
+
+    if [ "${line:0:1}" = "[" -a "${line: -1}" = "]" ]
+    then
+        line=`echo ${line%]*}`
+        line=`echo ${line##*[}`
+        link="${line}"
+        continue
+    fi
+
+    if [ -z "${link}" ]
+    then
+        continue
+    fi
+
+    name=`echo $line | awk '{print $1}'`
+    path=`echo $line | awk '{print $2}'`
+    priority=`echo $line | awk '{print $3}'`
+
+    update-alternatives --install ${link} ${name} ${path} ${priority}
+
+done < update-alternatives.conf
